2ea6455aea4dbb6e9a78a6a3063914a4d34c2ee7
kamellaidi <kamellaidi@outlook.fr>
2022-12-12 20:14:44 +0100

    new name

---
+++ front-end/src/components/Main/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Form from './Form'
+import Lists from './Lists'
+
+import './styles.css'
+
+const Main = () => {
+  return (
+    <main>
+    <Form />
+    <Lists />
+    </main>
+  )
+}
+
+export default Main
